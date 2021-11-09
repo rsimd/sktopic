65a2c2bf5eef0f36873b17427dca8b7e62e47a98
rsimd <rickysimd@gmail.com>
2021-11-09 06:26:53 +0000

    #4 sampling from prior for MMD

--- sktopic/components/mmd_loss.py
+++ sktopic/components/mmd_loss.py
@@ -1,10 +1,15 @@
 """
 original: https://github.com/zll17/Neural_Topic_Models/blob/master/models/wae.py#L89
 """
+from typing import Optional,Any
+from numpy.core.fromnumeric import size
 import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+from sklearn.mixture import GaussianMixture
 
 
-__all__ = ["mmd_loss_diffusion","mmd_loss_tv"]
+__all__ = ["mmd_loss_diffusion","mmd_loss_tv","prior_sample"]
 
 
 def diffusion_kernel(a:torch.Tensor, tmpt:float)->torch.Tensor:
@@ -82,3 +87,62 @@ def mmd_loss_tv(x:torch.Tensor,y:torch.Tensor)->torch.Tensor:
             sum_xy = sum_xy + torch.norm(x[i]-y[j], p=1).to(device)
     sum_yy = sum_yy / (M * y.shape[0])
     return sum_xx + sum_yy - sum_xy
+
+def prior_sample(
+    shape:tuple[int,int], 
+    dirichlet_alpha:float=0.1, 
+    encoded:Optional[torch.Tensor]=None, 
+    dtype:Any=torch.float32, 
+    dist:str='dirichlet',
+    )->torch.Tensor:
+    """sample from prior distribution
+
+    Parameters
+    ----------
+    shape : Tuple[int,int]
+        (batch_size, n_topics)
+    dirichlet_alpha : float, optional
+        dirichlet prior's hyper-parameter alpha, by default 0.1
+    encoded : Optional[torch.Tensor], optional
+        encoded vectors from encoder networks, by default None
+    dtype : Any, optional
+        data type on torch, by default torch.float32
+    dist : str, optional
+        name of prior distribution, by default 'dirichlet'
+
+    Returns
+    -------
+    torch.Tensor
+        [description]
+    """
+    M,K = shape
+    if dist == 'dirichlet':
+        concentration= torch.ones(shape,dtype=dtype)*dirichlet_alpha
+        z_true = torch.distributions.Dirichlet(concentration=concentration)
+        return z_true
+    
+    elif dist == 'gaussian':
+        logits = torch.randn(shape,dtype=dtype)
+        z_true = F.softmax(logits, dim=1)
+        return z_true
+    
+    elif dist == 'gmm_std':
+        odes = torch.eye(K,dtype=dtype)*20
+        ides = torch.randint(low=0,high=K, size=M)
+        mus = odes[ides]
+        sigmas = torch.ones(shape)*0.2*20
+        z_true = torch.randn(mus, sigmas)
+        z_true = F.softmax(z_true, dim=1)
+        return z_true
+    
+    elif dist=='gmm_ctm' and encoded!=None:
+        with torch.no_grad():
+            gmm = GaussianMixture(n_components=K,covariance_type='full',max_iter=200)
+            gmm.fit(encoded.detach().cpu().numpy())
+            #hid_vecs = torch.from_numpy(hid_vecs).to(self.device)
+            gmm_spls, _spl_lbls = gmm.sample(n_samples=len(encoded))
+            theta_prior = torch.from_numpy(gmm_spls).float()
+            theta_prior = F.softmax(theta_prior,dim=1)
+            return theta_prior
+    else:
+        return prior_sample(shape,dist='dirichlet')
