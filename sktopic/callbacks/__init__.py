f3ef402dac7ae8fae83af853f71ad6df516f975e
rsimd <rickysimd@gmail.com>
2021-11-24 05:36:10 +0000

    ...

--- sktopic/callbacks/__init__.py
+++ sktopic/callbacks/__init__.py
@@ -3,3 +3,4 @@ from .assignmentclusterlabels import AssignmentClusterlabelsByMyself
 #from .wecoherence import WECoherenceScoring
 from .perplexity import PerplexityScoring
 from .wecoherence import *
+from .nan_checker import NaNChecker
